9a0e1b0c2f979da3fecf5e3c1a695033ec9d4948
Moeez ahmed <ahmedmoeez7860@gmail.com>
2026-06-18 23:40:41 +0530

    Added Java solutions

---
+++ java/JavaInstructions.java
@@ -0,0 +1,6 @@
+public class JavapInstructions {
+    public static void main(String[] args) {
+        System.out.println("To inspect bytecode: compile with javac and run javap -c ClassName");
+        System.out.println("Example: javac HelloWorld.java && javap -c HelloWorld");
+    }
+}
