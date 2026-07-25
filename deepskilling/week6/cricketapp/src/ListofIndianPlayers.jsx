d5250154677af75091f3c224fca987ac3cefa21d
Moeez ahmed <ahmedmoeez7860@gmail.com>
2026-07-26 03:43:08 +0530

    cricketapp

---
+++ deepskilling/week6/cricketapp/src/ListofIndianPlayers.jsx
@@ -0,0 +1,13 @@
+function ListofIndianPlayers({ IndianPlayers }) {
+  return (
+    <ul>
+      {IndianPlayers.map((player, index) => (
+        <li key={index}>
+          Mr. {player}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ListofIndianPlayers;
